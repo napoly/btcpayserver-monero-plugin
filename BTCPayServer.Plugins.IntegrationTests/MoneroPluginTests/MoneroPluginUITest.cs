1600a58a8589cdfe4089e9149b51636d306217fd
napoly <napolytan@protonmail.com>
2025-05-05 20:50:53 +0200

    Create a happy path flow for a successful xmr plugin install

---
+++ BTCPayServer.Plugins.IntegrationTests/MoneroPluginTests/MoneroPluginUITest.cs
@@ -0,0 +1,25 @@
+using BTCPayServer.Tests;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace BTCPayServer.Plugins.IntegrationTests.MoneroPluginTests;
+
+public class MoneroPluginUITest(ITestOutputHelper helper) : MoneroIntegrationTestBase(helper)
+{
+    [Fact]
+    public async Task EnableMoneroPluginSuccessfully()
+    {
+        await using var s = CreatePlaywrightTester();
+        await s.StartAsync();
+        await s.RegisterNewUser(true);
+        await s.CreateNewStore();
+        await s.Page.Locator("a.nav-link[href*='monerolike/XMR']").ClickAsync();
+        await s.Page.Locator("#NewAccountLabel").FillAsync("Wallet Label");
+        await s.Page.CheckAsync("#Enabled");
+        await s.Page.SelectOptionAsync("#SettlementConfirmationThresholdChoice", "2");
+        await s.Page.ClickAsync("#SaveButton");
+        await s.Page.ReloadAsync();
+        var classList = await s.Page.Locator("svg.icon-checkmark").GetAttributeAsync("class");
+        Assert.Contains("text-success", classList);
+    }
+}
